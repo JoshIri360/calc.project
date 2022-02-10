b7e0cac0267e737357a5d8a957477ce78a05c107
JoshIri360 <91752742+JoshIri360@users.noreply.github.com>
2022-02-10 16:07:17 +0100

    almost done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+let answr = "";
+
 function addEl(Element) {
-  document.getElementById("inpt").value+=Element
+  document.getElementById("inpt").value+=Element;
+}
+
+function clr() {
+  document.getElementById("inpt").value="";
+}
+
+function eq()
+{
+ let x = document.getElementById("inpt").value;
+ answr = eval(x);
+ document.getElementById("inpt").value = answr;
+}
+
+let ans1 = () => {document.getElementById("inpt").value = answr}
+
+const allClear = () => {
+  clr();
+  answr = "";
 }
