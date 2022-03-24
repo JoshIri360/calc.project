eea99a60d6149e7439ed21ddb78434fdc1d65456
JoshIri360 <91752742+JoshIri360@users.noreply.github.com>
2022-03-24 11:51:53 +0100

    made it better

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let answr = "";
+var z = document.getElementById("inpt");
 
 function addEl(Element) {
   document.getElementById("inpt").value+=Element;
@@ -10,9 +11,9 @@ function clr() {
 
 function eq()
 {
- let x = document.getElementById("inpt").value;
- answr = eval(x);
- document.getElementById("inpt").value = answr;
+  let x = document.getElementById("inpt").value;
+  answr = eval(x);
+  document.getElementById("inpt").value = answr;
 }
 
 let ans1 = () => {document.getElementById("inpt").value = answr}
@@ -21,3 +22,9 @@ const allClear = () => {
   clr();
   answr = "";
 }
+
+input.addEventListener("keypress", function(e) {
+    if(e.keyCode === 13){
+        eq();
+    }
+})
